2c6bf89cff94eec45f30a1df3ae54c7bae4d3840
vishal <vishalpawar2550@gmail.com>
2024-11-14 15:03:18 +0530

    added project structure

---
+++ src/main/java/com/ContactNexus/Contollers/PageContoller.java
@@ -0,0 +1,37 @@
+package com.ContactNexus.Contollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.HashMap;
+import java.util.Map;
+@Controller
+public class PageContoller {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("Home");
+
+        // Create a map and add attributes
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put("name", "vvishal");
+
+        // Add the attributes to the model
+        model.addAllAttributes(attributes);
+
+        // Return the view name
+        return "home";
+    }
+
+
+    @RequestMapping("/about")
+    public String aboutPage(){
+        return "about";
+    }
+    @RequestMapping("/services")
+    public String servicePage(){
+        return "services";
+    }
+}
