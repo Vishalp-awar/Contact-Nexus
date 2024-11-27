67cc99ba84ca50bf012b11b7e075f001414fdacd
vishal <vishalpawar2550@gmail.com>
2024-11-28 03:45:30 +0530

    Added AddContact Form

--- src/main/java/com/ContactNexus/Contollers/ContactController.java
+++ src/main/java/com/ContactNexus/Contollers/ContactController.java
@@ -1,8 +1,12 @@
 package com.ContactNexus.Contollers;
 
 
+import com.ContactNexus.Forms.ContactForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/user/contacts")
@@ -13,7 +17,19 @@ public class ContactController {
         return "user/contact";
     }
     @RequestMapping("/add_contact")
-    public String addContactPage(){
+    public String addContactPage(Model model){
+        ContactForm contactForm=new ContactForm();
+        contactForm.setContactImage("this is imahge");
+        model.addAttribute("contactForm",contactForm);
         return "user/add_contact";
     }
+
+    @RequestMapping(value="add_contact" ,method = RequestMethod.POST)
+    public String SaveContact(@ModelAttribute ContactForm contactForm){
+        System.out.println(
+                contactForm
+        );
+        return "redirect:/user/contacts/add_contact";
+    }
+
 }
